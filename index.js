27fafa97f439ed49cc8c06813c6c5f3bb1084d2a
shiba6372 <159177596+shiba6372@users.noreply.github.com>
2024-03-02 23:46:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const router = require("express").Router()
-const Todo = require("../models/Todo");
+const Todo = require("./Todo");
 
 // routes will be here....
 router.get("/", async(req, res) => {
